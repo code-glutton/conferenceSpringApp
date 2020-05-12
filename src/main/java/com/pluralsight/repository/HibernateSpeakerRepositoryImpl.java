3a8fc58aaaa96535a0cba4ae96b40fdaa3482aa4
Jonathan-Odia Osahon Emmanuel <emmyodia@gmail.com>
2020-05-13 00:25:35 +0100

    spring scope and autowiring

--- src/main/java/com/pluralsight/repository/HibernateSpeakerRepositoryImpl.java
+++ src/main/java/com/pluralsight/repository/HibernateSpeakerRepositoryImpl.java
@@ -1,10 +1,12 @@
 package com.pluralsight.repository;
 
 import com.pluralsight.model.Speaker;
+import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Repository("speakerRepository")
 public class HibernateSpeakerRepositoryImpl implements SpeakerRepository {
 
     @Override
--- src/main/java/com/pluralsight/service/SpeakerServiceImpl.java
+++ src/main/java/com/pluralsight/service/SpeakerServiceImpl.java
@@ -3,21 +3,30 @@ package com.pluralsight.service;
 import com.pluralsight.model.Speaker;
 import com.pluralsight.repository.HibernateSpeakerRepositoryImpl;
 import com.pluralsight.repository.SpeakerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service("speakerService")
 public class SpeakerServiceImpl implements SpeakerService {
     private SpeakerRepository repository;
+
+    public SpeakerServiceImpl() {
+
+    }
 
     public SpeakerServiceImpl(SpeakerRepository repository) {
         this.repository = repository;
     }
+
 
     @Override
     public List<Speaker> findAll(){
     return repository.findAll();
     }
 
+    @Autowired
     public void setRepository(SpeakerRepository repository) {
         this.repository = repository;
     }
